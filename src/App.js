ff6cc6e3abc82f00637ba9ed9bcc241f50d4765e
dhruba-datta <dhrubadattaanjan@gmail.com>
2022-09-13 11:48:30 +0530

    all pages run perfectly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, Routes } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
@@ -12,7 +12,17 @@ const App = () => {
             <Navbar />
         </div>
         <div className="main">
-        
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/"  element={<Homepage />} />
+                <Route path="/exchanges"  element={<Exchanges />} />
+                <Route path="/cryptocurrencies"  element={<Cryptocurrencies />} />
+                <Route path="/crypto/:coinId"  element={<CryptoDetails />} />
+                <Route path="/news"  element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
         </div>
         <div className="footer">
             
