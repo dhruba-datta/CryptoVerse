2b758439d5f050131341c31b342ad201c8391576
dhruba-datta <dhrubadattaanjan@gmail.com>
2022-09-13 11:26:03 +0530

    3rd run succesful

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
-// import './App.css';
+import './App.css';
 
 const App = () => {
   return (
