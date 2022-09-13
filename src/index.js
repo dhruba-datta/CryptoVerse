81715810d1fc31e2e67e611d7f4eeb46833ee0ad
dhruba-datta <dhrubadattaanjan@gmail.com>
2022-09-13 11:06:02 +0530

    Started succesfully

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+// import store from './app/store';
+
+// import 'antd/dist/antd.css';
+
+ReactDOM.render(
+        <App />, document.getElementById('root')
+);
