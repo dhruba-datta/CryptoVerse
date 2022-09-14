dc27d057d8cfdcd308cf7535d5a1fe2b890fcf77
dhruba-datta <dhrubadattaanjan@gmail.com>
2022-09-14 10:27:28 +0530

    Before linechart everything done

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -12,9 +12,13 @@ export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-      getCryptos: builder.query({
-        query: (count) => createRequest(`/coins?limit=${count}`),
-      }),
+        getCryptos: builder.query({
+          query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+          query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        
     }),
   });
 
